3aa694eda03e34130b4a6dc0b664ac1abf09e4a1
Anton <antonio37rus@gmail.com>
2023-03-18 19:37:39 +0300

    Add files via upload

---
+++ src/components/search/index.jsx
@@ -0,0 +1,26 @@
+import "./styles.css";
+
+import { ReactComponent as CloseIcon } from "./assets/ic-close-input.svg";
+import { ReactComponent as SeachIcon } from "./assets/ic-search.svg";
+
+export function Search({ handleFormSubmit, handleInputChange }) {
+  return (
+    <form className="search" onSubmit={handleFormSubmit}>
+      <input
+        type="text"
+        className="search__input"
+         // parent (app/index) -> child (search/index)
+        onChange={(e) => {
+          handleInputChange(e.target.value);
+        }}
+        
+        placeholder="Поиск"
+      />
+      <button className="search__btn">
+        <SeachIcon />
+        <CloseIcon />
+      </button>
+    </form>
+  );
+}
+
---
+++ src/index.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line 
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './components/app';
+import './styles.css';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(<App />);
